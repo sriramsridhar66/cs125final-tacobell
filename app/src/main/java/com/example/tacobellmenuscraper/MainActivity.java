91d2640644dbd95e476aa3f5f7ab6998df510c7a
Sriram Sridhar <sriramsridhar66@gmail.com>
2020-04-17 17:16:26 -0700

    Initial commit

---
+++ app/src/main/java/com/example/tacobellmenuscraper/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tacobellmenuscraper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button loadMenuButton = findViewById(R.id.loadMenu);
+        loadMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Menu menu = new Menu();
+                try {
+                    menu.createMenuMap();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/tacobellmenuscraper/Menu.java
@@ -0,0 +1,37 @@
+package com.example.tacobellmenuscraper;
+
+import android.os.StrictMode;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Menu {
+    private Map<String, Double> menuMap = new HashMap<>();
+
+    public void createMenuMap() throws IOException {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Document menuDocument = Jsoup.connect("https://www.tacobell.com/food/tacos").get();
+        String title = menuDocument.title();
+        System.out.println(title);
+        Elements elements = menuDocument.select("div.product-details");
+        for (Element element : elements) {
+            String[] item = element.text().split("\\$");
+            menuMap.put(item[0].trim(), Double.valueOf(item[1].split(" ")[0].trim()));
+        }
+        for (Map.Entry<String, Double> entry: menuMap.entrySet()) {
+            System.out.println(entry.getKey() + " is $" + entry.getValue());
+        }
+    }
+
+    public Map<String, Double> getMenuMap() {
+        return menuMap;
+    }
+}
