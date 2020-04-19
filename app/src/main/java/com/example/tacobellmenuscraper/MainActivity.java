2780200c492425c8a20ddac518b19c4af0a4719b
keesanpatel <kpate360@illinois.edu>
2020-04-19 15:58:36 -0500

    tb logo added

--- app/src/main/java/com/example/tacobellmenuscraper/MainActivity.java
+++ app/src/main/java/com/example/tacobellmenuscraper/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button loadMenuButton = findViewById(R.id.loadMenu);
+        Button loadMenuButton = findViewById(R.id.start);
         loadMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
