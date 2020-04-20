c6406d1d0ee99f0be3ad36ab8a51347efd63398c
keesanpatel <kpate360@illinois.edu>
2020-04-20 14:40:14 -0500

    stored the getMoney value

--- app/src/main/java/com/example/tacobellmenuscraper/QuestionsPages.java
+++ app/src/main/java/com/example/tacobellmenuscraper/QuestionsPages.java
@@ -18,11 +18,21 @@ public class QuestionsPages extends AppCompatActivity {
 
         EditText optionsInput = findViewById(R.id.optionsInput);
         TextView whatToInput = findViewById(R.id.whatToInput);
-
         getMoney(whatToInput);
     }
 
-    private void getMoney(TextView whatToInput) {
+    private double getMoney(TextView whatToInput) {
         whatToInput.setText("Enter the amount of money you will be spending in Taco Bell today.");
+        EditText input = findViewById(R.id.optionsInput);
+        String temp = input.getText().toString();
+        double value = Double.parseDouble(temp);
+        return value;
+
+
     }
+    private void getDollarMenu(TextView whatToInput) {
+        whatToInput.setText("Enter the amount of dollar menu items you want.");
+    }
+
+
 }
