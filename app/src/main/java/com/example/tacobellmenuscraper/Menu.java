640f7b0f24fa4eb1457cf85d2c39cd45ac8830bf
Sriram Sridhar <sriramsridhar66@gmail.com>
2020-04-21 13:06:02 -0700

    started order generating logic methods

--- app/src/main/java/com/example/tacobellmenuscraper/Menu.java
+++ app/src/main/java/com/example/tacobellmenuscraper/Menu.java
@@ -1,7 +1,5 @@
 package com.example.tacobellmenuscraper;
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.os.StrictMode;
 
 import org.jsoup.Jsoup;
@@ -10,7 +8,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -31,11 +28,13 @@ public class Menu {
 
     private String[] URLs = {"https://www.tacobell.com/food/tacos", "https://www.tacobell.com/food/burritos", "https://www.tacobell.com/food/quesadillas"};
 
+
     public Menu(final double setMoneyAmount, final int setDollarMenuNumber, final boolean setDrinks) {
         moneyAmount = setMoneyAmount;
         dollarMenuNumber = setDollarMenuNumber;
         drinks = setDrinks;
     }
+
 
     public void loadMenu() throws IOException {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -49,24 +48,26 @@ public class Menu {
                 menuMap.put(item[0].trim(), Double.valueOf(item[1].split(" ")[0].trim()));
             }
         }
-
         for (HashMap.Entry<String, Double> entry : menuMap.entrySet()) {
             System.out.println(entry.getKey() + " is $" + entry.getValue());
         }
     }
 
+
     public HashMap<String, Double> getMenuMap() {
         return menuMap;
     }
 
+
     public HashMap<String, Double> getOrderMap() {
-        HashMap<String, Double> sortedMap = sortByAmount(menuMap);
+        menuMap = sortByAmount(menuMap);
         System.out.println("SORTED MAP:");
-        for (HashMap.Entry<String, Double> entry : sortedMap.entrySet()) {
+        for (HashMap.Entry<String, Double> entry : menuMap.entrySet()) {
             System.out.println(entry.getKey() + " is $" + entry.getValue());
         }
-        return sortedMap; //ADD LOGIC and return a new Map
+        return menuMap; //ADD LOGIC and return a new Map
     }
+
 
     private static HashMap<String, Double> sortByAmount(HashMap<String, Double> hm) {
         // Create a list from elements of HashMap
@@ -86,5 +87,30 @@ public class Menu {
             temp.put(aa.getKey(), aa.getValue());
         }
         return temp;
+    }
+
+
+    private HashMap<String, Double> getDollarMenu() {
+        //if [dollarMenuNumber] is 0, return empty HashMap
+        //randomly remove 6 - [dollarMenuNumber] dollar menu items from sortedMap
+        return null;
+    }
+
+    private HashMap<String, Double> getComboDrinks() {
+        //if drinks is false, return empty HashMap
+        //if drinks is true, randomly return one combo
+        return null;
+    }
+
+    private HashMap<String, Double> getRegularItems() {
+        //do logic to choose regular items
+        //return as a HashMap
+        return null;
+    }
+
+    private HashMap<String, Double> getFreeItems() {
+        //return free items as a Map
+        //can use to test generated order activity layout since free items will always be in the generated order.
+        return null;
     }
 }
